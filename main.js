f62f3a278679c230b634041a5350a3748c82cd21
Allard-Ivan <allard@gmail.com>
2019-06-02 20:47:44 +0800

    Update to vue project

--- main.js
+++
@@ -1,7 +0,0 @@
-function audioAutoPlay(id) {
-    var audio = document.getElementById(id);
-    audio.play();
-    document.addEventListener('WeixinJSBridgeReady', function() {
-        audio.play();
-    }, false);
-}
